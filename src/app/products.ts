eae8d68e5eb09a67351b08cf4d0eae21328d43af
andreywalker <98677791+andreywalker@users.noreply.github.com>
2023-02-22 22:04:21 +0600

    lab 4

---
+++ src/app/products.ts
@@ -0,0 +1,64 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  rating: number;
+  img: string;
+  description: string;
+  link: 'https://kaspi.kz/shop/p/watsons-mint-50-sht-108270150/?c=750000000#!/item';
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Ревит',
+    price: 799,
+    rating: 4.5,
+    img: 'https://images.satu.kz/163776127_revit-100drazhe-altajvitaminy.jpg',
+    description: 'Витаминки',
+    link: 'https://kaspi.kz/shop/p/altaivitaminy-revit-100-drazhe-100695785/?c=750000000#!/item',
+  },
+  {
+    id: 2,
+    name: 'Zubochistka',
+    price: 699,
+    rating: 1,
+    img: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hdc/h53/67427144564766/zubochistki-108270150-1.jpg',
+    description:
+      '50 штук зубочисток в пластиковом диспенсере. Производство - Китай',
+    link: 'https://kaspi.kz/shop/p/watsons-mint-50-sht-108270150/?c=750000000#!/item',
+  },
+  {
+    id: 3,
+    name: 'Курт',
+    price: 1,
+    rating: 100,
+    img: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h2b/ha6/45294250328094/ferma-orda-kurt-solenyj-110-g-102231155-1.jpg',
+    description: 'Лучшая еда на свете',
+    link: 'https://kaspi.kz/shop/p/ferma-orda-kurt-soljonyi-110-g-102231155/?c=750000000#!/item',
+  },
+  {
+    id: 4,
+    name: 'Apple Watch',
+    price: 514,
+    rating: 4.2,
+    img: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h1a/h69/63158968057886/apple-watch-series-8-41-mm-cernyj-106362847-1.jpg',
+    description: 'поддержка платформ: iOS материал корпуса: алюминийцвет корпуса: черный технология экрана: OLED объем встроенной памяти: 32 Гб интерфейсы: Bluetooth, ,Wi-Fi, ,NFC время работы: в обычном режиме: 18 часов, в режиме энергосбережения: 36 часов',
+    link: 'https://kaspi.kz/shop/p/apple-watch-series-8-45-mm-aluminum-chernyi-106362847/?c=750000000#!/item',
+  },
+  {
+    id: 5,
+    name: 'Тостер CENTEK СТ-1432 ',
+    price: 24.5,
+    rating: 5,
+    img: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h70/hf3/32725893414942/centek-st-1432-cernyj-100295343-1-Container.jpg',
+    description: 'тип управления: механическое количество тостов: 2 поддон для крошек: Да материал корпуса: пластик цвет: черный',
+    link: 'https://kaspi.kz/shop/p/centek-st-1432-chernyi-100295343/?c=750000000#!/item',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
